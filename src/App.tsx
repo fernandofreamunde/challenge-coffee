e2ce92c09d67c816da5417bc7408686d466c977a
Fernando Andrade <fernandofreamunde@gmail.com>
2023-03-25 14:40:15 +0100

    🎉➕➕➕ install react + initial dependencies

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import viteLogo from '../vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className="App">Coffeee</div>
+}
+
+export default App
